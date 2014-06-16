578e8e30bba13e9357f010824ae1ad5cfd561646
Feng.Xia <fengx@hp.com>
2014-06-16 21:05:33 +0800

    设计和实现用户考试答案相关表。

--- paper/models.py
+++ paper/models.py
@@ -20,18 +20,10 @@ class Question(models.Model):
 class Option(models.Model):
     serialno = models.CharField(max_length=32)
     content = models.CharField(max_length=1024)
-    type = models.CharField(max_length=32)
+    answer_type = models.CharField(max_length=32)
     question = models.ForeignKey(Question)
     def __unicode__(self):
         return u'%s' % (self.content) 
-    def toJSON(self):
-        fields = []
-        for field in self._meta.fields:
-            fields.append(field.name)
-        d = {}
-        for attr in fields:
-            d[attr] = getattr(self, attr)
-        return json.dumps(d)
     
 class Answer(models.Model):
     description = models.CharField(max_length=1024,null=True)
@@ -39,3 +31,18 @@ class Answer(models.Model):
     option = models.ForeignKey(Option,unique=True)
     def __unicode__(self):
         return u'%s %s' % (self.question, self.option)
+    
+class User_Paper(models.Model):
+    user_id = models.IntegerField()
+    paper = models.ForeignKey(Paper)
+    serialno = models.IntegerField()
+    def __unicode__(self):
+        return u'%s %s' % (self.userId, self.paper)    
+    
+class User_Answer(models.Model):
+    user_paper = models.ForeignKey(User_Paper)
+    question = models.ForeignKey(Question)
+    option = models.ForeignKey(Option)
+    def __unicode__(self):
+        return u'%s %s' % (self.question, self.option)  
+    
--- tool/db/create.sql
+++ tool/db/create.sql
@@ -1,9 +1,9 @@
 insert into paper_paper (id,name,description) values (1.0,'试卷1','试卷1');
 insert into paper_question (id,serialno,content,paper_id) values (1.0,1.0,'认识你的人倾向形容你为： ',1.0);
 insert into paper_question (id,serialno,content,paper_id) values (2.0,2.0,'下列哪一件事听起来比较吸引你？ ',1.0);
-insert into paper_option (id,serialno,content,type,question_id) values (1.0,'A','热情和敏感。','F',1.0);
-insert into paper_option (id,serialno,content,type,question_id) values (2.0,'B','逻辑和明确。','T',1.0);
-insert into paper_option (id,serialno,content,type,question_id) values (3.0,'A','与情人到有很多人且社交活动频繁的地方。','E',2.0);
-insert into paper_option (id,serialno,content,type,question_id) values (4.0,'B','呆在家中与情人做一些特别的事情，例如说观赏一部有趣的录影带并享用你最喜欢的外卖食物。','I',2.0);
+insert into paper_option (id,serialno,content,answer_type,question_id) values (1.0,'A','热情和敏感。','F',1.0);
+insert into paper_option (id,serialno,content,answer_type,question_id) values (2.0,'B','逻辑和明确。','T',1.0);
+insert into paper_option (id,serialno,content,answer_type,question_id) values (3.0,'A','与情人到有很多人且社交活动频繁的地方。','E',2.0);
+insert into paper_option (id,serialno,content,answer_type,question_id) values (4.0,'B','呆在家中与情人做一些特别的事情，例如说观赏一部有趣的录影带并享用你最喜欢的外卖食物。','I',2.0);
 insert into paper_answer (id,description,question_id,option_id) values (1.0,'题目1答案',1.0,1.0);
 insert into paper_answer (id,description,question_id,option_id) values (2.0,'题目2答案',2.0,3.0);
